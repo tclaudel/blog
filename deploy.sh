a253decc1f5df5a96d821d9f2ee0c10d39c0cd20
tclaudel <zekidok@gmail.com>
2020-06-02 17:30:28 +0200

    deployed on

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,13 @@
 rm -Rf ./public
 hugo
 git add .
+git status
+read -p "Continue ? (y/n) " -n 1 -r
+echo    # (optional) move to a new line
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    exit 1
+fi
 DATE= date +'%d/%m/%y'
 echo $DATE
 git commit -m "deployed on $DATE"
